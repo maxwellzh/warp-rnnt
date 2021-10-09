b6f741406722093910f6c9ba4259b45000503d56
maxwellzh <maxwellzh@outlook.com>
2021-10-09 16:44:34 +0800

    add argument checks for fused_rnnt_loss

--- pytorch_binding/warp_rnnt/__init__.py
+++ pytorch_binding/warp_rnnt/__init__.py
@@ -183,6 +183,14 @@ def fused_rnnt_loss(logits: torch.FloatTensor,
     assert not frames_lengths.requires_grad, "frames_lengths does not require gradients"
     assert not labels_lengths.requires_grad, "labels_lengths does not require gradients"
 
+    assert logits.dim(
+    ) == 2, f"Logits should be of shape (Stu, V), instead of {logits.size()}"
+    assert labels.dim(
+    ) == 1, f"Labels should be of shape (Su, ), instead of {labels.size()}"
+    assert frames_lengths.dim() == 1
+    assert labels_lengths.dim() == 1
+    assert frames_lengths.size(0) == labels_lengths.size(0)
+
     costs = RNNTLossFusion.apply(
         logits, labels, frames_lengths, labels_lengths, blank)
 
